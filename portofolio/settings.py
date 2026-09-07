b7173d2e20a27260c17be7031bee0aa6ed23bec1
umar <umar.faiz@ui.ac.id>
2026-09-07 21:13:11 +0700

    Fix database configuration to sqlite3

--- portofolio/settings.py
+++ portofolio/settings.py
@@ -105,8 +105,8 @@ if PRODUCTION:
 else:
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
+           'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
         }
     }
 
