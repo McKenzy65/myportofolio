c834aec9188c6bad46759cf19aeb27241ff42f14
umar <umar.faiz@ui.ac.id>
2026-09-14 21:31:36 +0700

    feat: register Certification model in Django admin

--- main/admin.py
+++ main/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from main.models import Experience
+from main.models import Experience, Certification
 
 admin.site.register(Experience)
+admin.site.register(Certification)
